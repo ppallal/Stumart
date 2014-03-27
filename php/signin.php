a61d14b0c0b36e8b34be19f34eae30170f1bb85f
Ronnel Maclean Pinto <ronnel.pinto@gmail.com>
2014-03-27 13:10:44 +0530

    Some PHP codes I wrote
    These are the untested php codes for some of the database functions

---
+++ php/signin.php
@@ -0,0 +1,31 @@
+
+<?php
+require'connect.php';
+$username=$_POST["username"];
+$password=(md5($_POST["password"]));
+$query = "SELECT LoginID FROM login WHERE Username = '{$username}' and Password='{$password}'";
+
+                   if($query_run=mysql_query($query))
+                  {
+						if(mysql_num_rows($query_run)==0)
+					{
+						echo "Enter valid username and password";
+						
+					}
+					else
+					{
+                        while($query_row=mysql_fetch_assoc($query_run))
+                        {
+                              $LoginID=$query_row['LoginID'];
+                            
+                        }
+							// enter the appropriate page to go to after login
+							//	header('Location:web_edititems.php?n='.$LoginID);
+					}
+				  }
+                  else
+                  {
+                      echo mysql_error();
+                  }
+
+?>
