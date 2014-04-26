b811b9774592b8afe5ff31f2f06f7198233f9d90
konarkjs <konarkjs@gmail.com>
2014-04-26 13:31:18 +0530

    Product Search

--- Scripts/Update_Product.php
+++ Scripts/Update_Product.php
@@ -4,5 +4,8 @@ require ‘connect.php’;
 $ProductName = $_POST[‘ProductName’];
 $IsBook = $_POST[‘IsBook’];
 $select_query = INSERT INTO `products`(`ProductName`, `IsBook`) VALUES (‘{$ProductName}’, ‘{$IsBook}’);
+$select_query_run = mysql_query($select_query);
+if(!$select_query_run)
+	echo mysql_error();
 #Should add a redirection
 ?>
